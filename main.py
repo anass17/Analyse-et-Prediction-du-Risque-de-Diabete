c69f7dc862c14b04d8627f15e7da5727adea3a7a
anass17 <anassboutaib2018@gmail.com>
2025-12-04 17:26:13 +0100

    Ajouter le fichier requirements.txt

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 
 model = load(os.getcwd() + '/models/model.pkl')
+scaler = load(os.getcwd() + '/models/scaler.pkl')
 
-st.title("Predict Risk of diabet")
+st.title("Prédire le risque de diabète")
 
 age = st.slider("Age", 0, 100)
 pregnancy = st.slider("Pregnancy", 0, 100)
@@ -18,28 +19,32 @@ skin = st.number_input("SkinThickness")
 bmi = st.number_input("BMI")
 function = st.number_input("DiabetesPedigreeFunction")
 
-if st.button("Predict"):
+if st.button("Prédire"):
     if (age and pregnancy and glucose and insulin and blood and skin and bmi and function):
         input_data = {
-            "age": [np.log(age)],
-            "pregnancy" : [np.log(pregnancy)],
-            "glucose" : [glucose],
-            "insulin" : [np.log(insulin)],
-            "blood" : [blood],
-            "skin" : [skin],
-            "bmi" : [bmi],
-            "function" : [np.log(function)],
+            "Pregnancies" : [np.log(pregnancy)],
+            "Glucose" : [glucose],
+            "BloodPressure" : [blood],
+            "SkinThickness" : [skin],
+            "Insulin" : [np.log(insulin)],
+            "BMI" : [bmi],
+            "DiabetesPedigreeFunction" : [np.log(function)],
+            "Age": [np.log(age)],
         }
 
-        scaler = StandardScaler()
-        X_std = scaler.fit_transform(input_data)
+        input_df = pd.DataFrame(input_data)
 
-        input_df = pd.DataFrame(X_std)
+        X_std = scaler.transform(input_df)
 
-        pred = model.predict(input_df)
+        pred = model.predict(X_std)
 
+        st.divider()
+        st.subheader("Le résultat")
 
-        st.text(pred)
+        if pred[0] == 1:
+            st.error("Le risque d'avoir le diabete est: **Eleve**")
+        else:
+            st.success("Le risque d'avoir le diabete est: **Faible**")
 
 
     else:
